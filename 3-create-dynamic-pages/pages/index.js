2262356dfd3acf155bcfdc4031de5ac5b7277d40
Emily Ritter <ritter@gcgmarketing.com>
2019-05-30 09:45:54 -0500

    End of 'Create Dynamic Pages' lesson

--- 3-create-dynamic-pages/pages/index.js
+++ 3-create-dynamic-pages/pages/index.js
@@ -1,9 +1,23 @@
 import Layout from '../components/MyLayout.js'
+import Link from 'next/link'
+
+const PostLink = props => (
+    <li>
+      <Link href={`/post?title=${props.title}`}>
+        <a>{props.title}</a>
+      </Link>
+    </li>
+)
 
 export default function Index() {
   return (
     <Layout>
-      <p>Hello Next.js</p>
+      <h1>My blog</h1>
+      <ul>
+        <PostLink title="Hello Next.js" />
+        <PostLink title="Learning Next.js is awesome" />
+        <PostLink title="Deploy apps with Zeit" />
+      </ul>
     </Layout>
   )
 }
